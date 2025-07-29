1171bc7cb3276b657258051c4ffaa2f3fd5fe39d
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-07-29 16:49:50 +0700

    update database for admin

--- EShop.sql
+++ EShop.sql
@@ -1,182 +1,111 @@
+-- ========= BẢNG QUẢN LÝ NGƯỜI DÙNG =========
+
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_deleted BOOL NOT NULL DEFAULT FALSE,
-    deleted_at INT,
-    deleted_by INT
+    email VARCHAR(100) NOT NULL UNIQUE,
+    fullname VARCHAR(100) NOT NULL,
+    address VARCHAR(255),
+    phone_number VARCHAR(15),
+    role VARCHAR(10) NOT NULL CHECK (role IN ('Admin', 'Seller', 'Buyer')) DEFAULT 'Buyer',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    deleted_at TIMESTAMP WITH TIME ZONE
 );
 
-CREATE TABLE profile (
-    profile_id INT UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    address VARCHAR(100) NOT NULL,
-    fullname VARCHAR(50) NOT NULL,
-    birthday TIMESTAMP,
-    sex CHAR(10) CHECK (sex IN ('Male', 'Female', 'Other')),
-    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
+-- ========= BẢNG QUẢN LÝ SẢN PHẨM =========
 
 CREATE TABLE categories (
     category_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE product (
+CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    description VARCHAR(1000),
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
+    stock_quantity INT NOT NULL CHECK (stock_quantity >= 0),
+    image_url VARCHAR(255),
     category_id INT NOT NULL,
-    remaining_number INT NOT NULL,
-    seller_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_deleted BOOL NOT NULL DEFAULT FALSE,
-    deleted_by INT
+    seller_id INT NOT NULL, -- Liên kết với người bán
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    
+    CONSTRAINT fk_product_category FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    CONSTRAINT fk_product_seller FOREIGN KEY (seller_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE product_images (
+    image_id SERIAL PRIMARY KEY,
+    product_id INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_image_product FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
 );
 
 CREATE TABLE product_reviews (
+    review_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     product_id INT NOT NULL,
-    number_stars INT NOT NULL CHECK (number_stars BETWEEN 1 AND 5),
-    description VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_deleted BOOL DEFAULT FALSE NOT NULL,
-    deleted_at TIMESTAMP,
-    deleted_by INT,
-    PRIMARY KEY (user_id, product_id)
+    rating INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_review_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_review_product FOREIGN KEY (product_id) REFERENCES products(product_id),
+    UNIQUE (user_id, product_id) -- Mỗi người dùng chỉ đánh giá 1 lần cho 1 sản phẩm
 );
 
-CREATE TABLE shopping_carts (
-    cart_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    total INT NOT NULL,
-    state CHAR(10) CHECK (state IN ('In_progress', 'Cancelled', 'Completed')) NOT NULL DEFAULT 'In_progress',
-    updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_deleted BOOL NOT NULL DEFAULT FALSE,
-    deleted_at TIMESTAMP,
-    deleted_by INT
-);
+-- ========= BẢNG QUẢN LÝ GIỎ HÀNG =========
 
 CREATE TABLE cart_items (
-    item_id SERIAL PRIMARY KEY,
-    cart_id INT NOT NULL,
+    cart_item_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
     product_id INT NOT NULL,
-    is_deleted BOOL NOT NULL DEFAULT FALSE,
-    deleted_at TIMESTAMP,
-    deleted_by INT
+    quantity INT NOT NULL CHECK (quantity > 0),
+    added_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_cart_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_cart_product FOREIGN KEY (product_id) REFERENCES products(product_id),
+    UNIQUE (user_id, product_id) -- Đảm bảo mỗi sản phẩm chỉ có một dòng trong giỏ hàng của người dùng
 );
 
-CREATE TABLE payment (
-    payment_id SERIAL PRIMARY KEY,
-    checkout_id INT NOT NULL,
-    payment_method_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    is_deleted TIMESTAMP,
-    deleted_by INT
-);
+-- ========= BẢNG QUẢN LÝ ĐƠN HÀNG VÀ THANH TOÁN =========
 
 CREATE TABLE payment_methods (
     payment_method_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE CHECK (name IN ('credit card', 'cash', 'paypal')),
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    name VARCHAR(50) NOT NULL UNIQUE CHECK (name IN ('Cash on Delivery', 'Bank Transfer'))
 );
 
-CREATE TABLE checkout (
-    checkout_id SERIAL PRIMARY KEY,
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    cart_id INT NOT NULL,
     receiver_name VARCHAR(100) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    phone_number CHAR(15) NOT NULL,
-    email VARCHAR(50) NOT NULL
+    shipping_address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    status VARCHAR(20) NOT NULL CHECK (status IN ('Pending', 'Shipped', 'Delivered', 'Cancelled')) DEFAULT 'Pending',
+    payment_method_id INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_order_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT fk_order_payment_method FOREIGN KEY (payment_method_id) REFERENCES payment_methods(payment_method_id)
 );
 
-ALTER TABLE profile
-ADD CONSTRAINT fk_profile_users
-FOREIGN KEY (profile_id)
-REFERENCES users(user_id);
+CREATE TABLE order_items (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price_at_purchase DECIMAL(10, 2) NOT NULL, -- Lưu lại giá tại thời điểm mua
 
-ALTER TABLE users
-ADD CONSTRAINT fk_users_deleted_by
-FOREIGN KEY (deleted_by)
-REFERENCES users(user_id);
-
-ALTER TABLE product
-ADD CONSTRAINT fk_product_deleted_by
-FOREIGN KEY (deleted_by)
-REFERENCES users(user_id);
-
-ALTER TABLE product
-ADD CONSTRAINT fk_product_is_sold
-FOREIGN KEY (seller_id)
-REFERENCES users(user_id);
-
-ALTER TABLE product
-ADD CONSTRAINT fk_product_in_catagory
-FOREIGN KEY (category_id)
-REFERENCES categories(category_id);
-
-ALTER TABLE product_reviews
-ADD CONSTRAINT fk_reviews_deleted_by
-FOREIGN KEY (deleted_by)
-REFERENCES users(user_id);
-
-ALTER TABLE shopping_carts
-ADD CONSTRAINT fk_carts_deleted_by
-FOREIGN KEY (deleted_by)
-REFERENCES users(user_id);
-
-ALTER TABLE cart_items
-ADD CONSTRAINT fk_cart_items_deleted_by
-FOREIGN KEY (deleted_by)
-REFERENCES users(user_id);
-
-ALTER TABLE payment
-ADD CONSTRAINT fk_payment_deleted_by
-FOREIGN KEY (deleted_by)
-REFERENCES users(user_id);
-
-ALTER TABLE product_reviews
-ADD CONSTRAINT fk_reivews_by_user
-FOREIGN KEY (user_id)
-REFERENCES users(user_id);
-
-ALTER TABLE product_reviews
-ADD CONSTRAINT fk_reivews_product
-FOREIGN KEY (user_id)
-REFERENCES product(product_id);
-
-ALTER TABLE shopping_carts
-ADD CONSTRAINT fk_shopping_of_user
-FOREIGN KEY (user_id)
-REFERENCES users(user_id);
-
-ALTER TABLE cart_items
-ADD CONSTRAINT fk_item_of_cart
-FOREIGN KEY (cart_id)
-REFERENCES shopping_carts(cart_id);
-
-ALTER TABLE cart_items
-ADD CONSTRAINT fk_item_is_product
-FOREIGN KEY (product_id)
-REFERENCES product(product_id);
-
-ALTER TABLE payment
-ADD CONSTRAINT fk_payment_method
-FOREIGN KEY (payment_method_id)
-REFERENCES payment_methods(payment_method_id);
-
-ALTER TABLE payment
-ADD CONSTRAINT fk_payment_of_checkout
-FOREIGN KEY (checkout_id)
-REFERENCES checkout(checkout_id);
-
-ALTER TABLE checkout
-ADD CONSTRAINT fk_checkout_of_cart
-FOREIGN KEY (cart_id)
-REFERENCES shopping_carts(cart_id);
+    CONSTRAINT fk_order_item_order FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    CONSTRAINT fk_order_item_product FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
