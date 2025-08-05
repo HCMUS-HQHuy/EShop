39c5182c506eb18ca35b6b35b3d5479a4e7f03b4
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-08-05 16:59:35 +0700

    refactor: consolidate validation logic for user credentials and registration into auth.controller, removing utility files

--- src/utils/index.util.ts
+++ src/utils/index.util.ts
@@ -1,5 +1,3 @@
-export * from "./validateCredentials.util";
-export * from "./validateRegistration.util";
 export * from "./validateCategories.util";
 export * from "./password.util";
 export * from "./validateUpdateData.util";
