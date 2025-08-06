12cb98ad87ec5bbefc5a3b6e2f633f860e0cfa58
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-08-06 09:20:00 +0700

    refactor: reorganize validation functions for product data and filters, enhancing clarity and maintainability

--- src/controllers/seller/products.seller.controller.ts
+++ src/controllers/seller/products.seller.controller.ts
@@ -4,6 +4,8 @@ import { getConnection, releaseConnection } from '../../config/db';
 import * as utils from '../../utils/index.util';
 import * as types from '../../types/index.types';
 
+// #### VALIDATION FUNCTIONS ####
+
  function validateProductFilters(req: types.RequestCustom): types.ValidationProductResult {
     const errors: Partial<Record<keyof types.ProductFilterParams | keyof types.ProductParamsRequest, string>> = {};
 
@@ -34,6 +36,30 @@ import * as types from '../../types/index.types';
     };
 }
 
+function validateProductData(data: types.ProductAddRequest): types.ValidationProductResult {
+    const errors: Partial<Record<keyof types.ProductAddRequest, string>> = {};
+    if (data?.name === undefined || data.name.trim() === '') {
+        errors.name = 'Name is required';
+    }
+
+    if (data?.price === undefined || data.price < 0) {
+        errors.price = 'Price must be a non-negative number';
+    }
+
+    if (data?.stock_quantity === undefined || data.stock_quantity < 0) {
+        errors.stock_quantity = 'Stock quantity must be a non-negative number';
+    }
+
+    if (data?.category_id === undefined || data.category_id <= 0) {
+        errors.category_id = 'Category ID must be a positive number';
+    }
+    return { 
+        valid: Object.keys(errors).length === 0, 
+        errors
+    };
+}
+
+// #### HELPER FUNCTIONS ####
 function getFilterParamsForProducts(req: types.RequestCustom): types.ProductParamsRequest  {
     const params: types.ProductParamsRequest = {
         page: req.query.page !== undefined ? Number(req.query.page) : Number(process.env.PAGINATION_DEFAULT_PAGE),
@@ -48,6 +74,7 @@ function getFilterParamsForProducts(req: types.RequestCustom): types.ProductPara
     return params;
 }
 
+// #### DATABASE FUNCTIONS ####
 async function listProducts(params: types.ProductParamsRequest) {
     let db: Client | undefined = undefined;
     try {
@@ -85,6 +112,28 @@ async function listProducts(params: types.ProductParamsRequest) {
     }
 }
 
+async function addProduct(product: types.ProductAddRequest) {
+    console.log("Product added:", product);
+    let db: Client | undefined = undefined;
+    try {
+        db = await getConnection();
+        const sql = `
+            INSERT INTO products (name, price, stock_quantity, category_id, shop_id)
+            VALUES ($1, $2, $3, $4, $5)
+        `;
+        const data = [product.name, product.price, product.stock_quantity, product.category_id, product.shop_id];
+        await db.query(sql, data);
+    } catch (error) {
+        console.error('Error adding product to database:', error);
+        throw error;
+    } finally {
+        if (db) {
+            releaseConnection(db);
+        }
+    }
+}
+
+// #### CONTROLLER FUNCTIONS ####
 async function list(req: types.RequestCustom, res: express.Response) {
     if (utils.isSeller(req) === false) {
         return res.status(403).send({ error: 'Forbidden: Only sellers can list products' });
@@ -107,50 +156,6 @@ async function list(req: types.RequestCustom, res: express.Response) {
     } catch (error) {
         console.error('Error listing products:', error);
         res.status(500).send({ error: 'Internal server error' });
-    }
-}
-
-function validateProductData(data: types.ProductAddRequest): types.ValidationProductResult {
-    const errors: Partial<Record<keyof types.ProductAddRequest, string>> = {};
-    if (data?.name === undefined || data.name.trim() === '') {
-        errors.name = 'Name is required';
-    }
-
-    if (data?.price === undefined || data.price < 0) {
-        errors.price = 'Price must be a non-negative number';
-    }
-
-    if (data?.stock_quantity === undefined || data.stock_quantity < 0) {
-        errors.stock_quantity = 'Stock quantity must be a non-negative number';
-    }
-
-    if (data?.category_id === undefined || data.category_id <= 0) {
-        errors.category_id = 'Category ID must be a positive number';
-    }
-    return { 
-        valid: Object.keys(errors).length === 0, 
-        errors
-    };
-}
-
-async function addProduct(product: types.ProductAddRequest) {
-    console.log("Product added:", product);
-    let db: Client | undefined = undefined;
-    try {
-        db = await getConnection();
-        const sql = `
-            INSERT INTO products (name, price, stock_quantity, category_id, shop_id)
-            VALUES ($1, $2, $3, $4, $5)
-        `;
-        const data = [product.name, product.price, product.stock_quantity, product.category_id, product.shop_id];
-        await db.query(sql, data);
-    } catch (error) {
-        console.error('Error adding product to database:', error);
-        throw error;
-    } finally {
-        if (db) {
-            releaseConnection(db);
-        }
     }
 }
 
