0081d535aab07f17a5c829f96b74e3422a644e7e
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-08-17 10:34:38 +0000

    Clarify the type of the 'credentials' variable.

---
+++ frontend/src/Features/userSlice.tsx
@@ -0,0 +1,105 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../Api/index.api.js";
+import type { Credentials } from "Types/credentials.ts";
+
+const initialState = {
+  loginInfo: {
+    username: "",
+    email: "",
+    password: "",
+    address: "",
+    phoneNumber: "",
+    isSignIn: false,
+  },
+  status: 'idle'
+}
+
+export const newSignUp = createAsyncThunk(
+  "user/newSignUp",
+  async (userInfor) => {
+    const response = await api.user.signUp(userInfor);
+    return response.data;
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "user/login",
+  async (credentials: Credentials) => {
+    const response = await api.user.login(credentials);
+    return response.data;
+  }
+);
+
+export const signOut = createAsyncThunk (
+  "user/signOut",
+  async () => {
+
+  }
+);
+
+export const setLoginData = createAsyncThunk(
+  "user/getLoginData",
+  async () => {
+    const response = await api.user.getInfor();
+    return response.data;
+  }
+);
+
+const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  reducers: {
+    // newSignUp: (state, { payload }) => {
+    //   state.signedUpUsers = payload;
+    //   state.loginInfo.isSignIn = true;
+    // },
+    // setLoginData: (state, { payload }) => {
+    //   state.loginInfo = { ...payload };
+    //   state.loginInfo.isSignIn = true;
+    // },
+    // signOut: (state) => {
+    //   const guestData = {
+    //     username: "",
+    //     emailOrPhone: "",
+    //     password: "",
+    //   };
+    //   state.loginInfo = guestData;
+    //   state.loginInfo.isSignIn = false;
+    // },
+    updateUserData: (state, { payload }) => {
+      Object.assign(state.loginInfo, payload.updatedUserData);
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.status = 'idle';
+      console.log("User logged in:", action.payload);
+    })
+    builder.addCase(loginUser.pending, (state) => {
+      state.status = 'pending';
+      console.log("Logging in user...");
+    })
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.status = 'idle';
+      console.error("User login failed:", action.error);
+    })
+
+    builder.addCase(setLoginData.fulfilled, (state, action) => {
+      state.loginInfo = { ...action.payload };
+      state.loginInfo.isSignIn = true;
+      console.log("User login info updated:", state.loginInfo);
+    });
+    builder.addCase(setLoginData.pending, (state) => {
+      state.status = 'pending';
+      // console.log("Fetching user login data...");
+    });
+    builder.addCase(setLoginData.rejected, (state, action) => {
+      state.status = 'idle';
+      console.error("Fetching user login data failed:", action.error);
+    });
+  }
+});
+
+export const { updateUserData } =
+  userSlice.actions;
+export default userSlice.reducer;
