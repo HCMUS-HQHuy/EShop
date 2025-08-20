5254055a6c7fd3646913f75cab74b1cc4c9759f0
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-08-20 04:57:02 +0000

    refactor: use issues attirbute of zod instead of format method

--- backend/src/utils/formatError.utils.ts
+++ backend/src/utils/formatError.utils.ts
@@ -2,13 +2,11 @@ import type { ZodError } from "zod";
 
 export function formatError(error: ZodError<any>) {
     const formattedErrors: Record<string, string> = {};
-    const fieldErrors = error.format();
-
-    for (const key in fieldErrors) {
-      if (key !== "_errors" && fieldErrors[key]?._errors?.length > 0) {
-        formattedErrors[key] = fieldErrors[key]._errors[0];
-      }
-    }
+    const fieldErrors = error.issues;
+    fieldErrors.forEach(issue => {
+        const path = issue.path.join('.');
+        formattedErrors[path] = issue.message;
+    });
     return formattedErrors;
 }
 
