8b8a77bd4cfef5cf44b361ac4109e019839187af
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-08-20 07:49:20 +0000

    refactor: Update checkRole for more flexibility.

--- backend/src/utils/checkRole.utils.ts
+++ backend/src/utils/checkRole.utils.ts
@@ -1,35 +1,35 @@
 import * as types from "../types/index.types";
 
-export function isSeller(req: types.RequestCustom): boolean {
-    if (!req.user || !req.user.shop_id) {
+export function isSeller(user: types.UserInfor | undefined): boolean {
+    if (!user || !user.shop_id) {
         return false;
     }
     return true;
 }
 
-export function isAcceptedSeller(req: types.RequestCustom): boolean {
-    if (!req.user || !req.user.shop_status) {
+export function isAcceptedSeller(user: types.UserInfor | undefined): boolean {
+    if (!user || !user.shop_status) {
         return false;
     }
-    return req.user.shop_status === types.SHOP_STATUS.ACTIVE || req.user.shop_status === types.SHOP_STATUS.CLOSED;
+    return user.shop_status === types.SHOP_STATUS.ACTIVE || user.shop_status === types.SHOP_STATUS.CLOSED;
 }
 
-export function isAdmin(req: types.RequestCustom): boolean {
-    if (!req.user || !req.user.role) {
+export function isAdmin(user: types.UserInfor | undefined): boolean {
+    if (!user || !user.role) {
         return false;
     }
-    return req.user.role === types.USER_ROLE.ADMIN;
+    return user.role === types.USER_ROLE.ADMIN;
 }
 
-export function isUser(req: types.RequestCustom): boolean {
-    if (!req.user || !req.user.role) {
+export function isUser(user: types.UserInfor | undefined ): boolean {
+    if (!user || !user.role) {
         return false;
     }
-    return req.user.role === types.USER_ROLE.USER;
+    return user.role === types.USER_ROLE.USER;
 }
 
-export function isGuest(req: types.RequestCustom): boolean {
-    return !req.user || !req.user.role;
+export function isGuest(user: types.UserInfor | undefined): boolean {
+    return !user;
 }
 
 const role = {
