41dca0c44778ea62494224e7f76a07e28093e4c2
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-08-20 04:49:20 +0000

    refactor: update methods

--- backend/src/utils/index.utils.ts
+++ backend/src/utils/index.utils.ts
@@ -1,15 +1,17 @@
 export * from "./password.utils";
-export * from "./common.utils";
+export * from "./checkRole.utils";
 export * from "./validate.utils";
 export * from "./gencode.utils";
 export * from "./formatError.utils"
 import formatError  from "./formatError.utils";
 import password from "./password.utils";
 import response from "./response.utils";
+import role from "./checkRole.utils";
 
 const util = {
     formatError,
     password,
-    response
+    response,
+    role
 }
 export default util;
