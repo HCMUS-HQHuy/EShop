765416030171105e2a72e094b5532e200153175a
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-08-27 02:30:05 +0000

    feat: add success message for product added to cart and update type definitions

--- frontend/src/Components/ProductDetailsPage/ProductDetails/ProductDealingControls/BuyButton/BuyButton.tsx
+++ frontend/src/Components/ProductDetailsPage/ProductDetails/ProductDealingControls/BuyButton/BuyButton.tsx
@@ -1,16 +1,18 @@
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
-import { showAlert } from "src/Features/alertsSlice";
-import { addToArray } from "src/Features/productsSlice";
-import { compareDataByObjValue } from "src/Functions/conditions";
+import { showAlert } from "src/Features/alertsSlice.tsx";
+import { addToArray, storeToStorage } from "src/Features/productsSlice.tsx";
+import { compareDataByObjValue } from "src/Functions/conditions.ts";
 import s from "./BuyButton.module.scss";
+import type { RootState } from "src/Types/store.ts";
+import { STORAGE_KEYS } from "src/Types/common.ts";
 
 const BuyButton = () => {
   const { selectedProduct, productQuantity, cartProducts, orderProducts } =
-    useSelector((state) => state.products);
+    useSelector((state: RootState) => state.products);
   const {
     loginInfo: { isSignIn },
-  } = useSelector((state) => state.user);
+  } = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
@@ -55,9 +57,17 @@ const BuyButton = () => {
         value: clonedProduct,
       })
     );
+    dispatch(
+      showAlert({
+        alertText: t(`toastAlert.productAddedToCart`),
+        alertState: "success",
+        alertType: "alert",
+      })
+    );
+    dispatch(storeToStorage({ key: STORAGE_KEYS.CART_PRODUCTS }));
   }
 
-  function showWarning(translateKey) {
+  function showWarning(translateKey: string) {
     dispatch(
       showAlert({
         alertText: t(`toastAlert.${translateKey}`),
--- frontend/src/Components/ProductDetailsPage/ProductDetails/ProductDealingControls/ProductDealingControls.tsx
+++ frontend/src/Components/ProductDetailsPage/ProductDetails/ProductDealingControls/ProductDealingControls.tsx
@@ -1,9 +1,14 @@
-import AddToFavButton from "./AddToFavButton/AddToFavButton";
-import BuyButton from "./BuyButton/BuyButton";
-import CustomNumberInput from "./CustomNumberInput/CustomNumberInput";
+import AddToFavButton from "./AddToFavButton/AddToFavButton.tsx";
+import BuyButton from "./BuyButton/BuyButton.tsx";
+import CustomNumberInput from "./CustomNumberInput/CustomNumberInput.tsx";
 import s from "./ProductDealingControls.module.scss";
+import type { ProductDetailType } from "src/Types/product.ts";
 
-const ProductDealingControls = ({ productData }) => {
+type Props = {
+  productData: ProductDetailType;
+}
+
+const ProductDealingControls = ({ productData }: Props) => {
   return (
     <section className={s.dealing}>
       <CustomNumberInput />
