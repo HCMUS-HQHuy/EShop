67d879b3b30668a89b855d025644ace33aa99433
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-09-03 04:12:53 +0000

    fix: persist global state to localStorage on update

--- frontend/src/Features/globalSlice.tsx
+++ frontend/src/Features/globalSlice.tsx
@@ -1,15 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { APP_MODE } from "src/Types/common.ts";
 
-const initialState = {
-  isOverlayActive: false,
-  isMobileMenuActive: false,
-  isProfileMenuActive: false,
-  isSectionsMenuActive: false,
-  isZoomInPreviewActive: false,
-  previewImg: null,
-  appMode: APP_MODE.USER
-};
+const globalStateStr = localStorage.getItem("globalState");
+const initialState = globalStateStr
+  ? JSON.parse(globalStateStr)
+  : {
+      isOverlayActive: false,
+      isMobileMenuActive: false,
+      isProfileMenuActive: false,
+      isSectionsMenuActive: false,
+      isZoomInPreviewActive: false,
+      previewImg: null,
+      appMode: APP_MODE.USER
+  };
 
 const globalSlice = createSlice({
   initialState,
@@ -20,6 +23,7 @@ const globalSlice = createSlice({
     },
     updateGlobalState: (state, { payload: { key, value } }) => {
       state[key] = value;
+      localStorage.setItem("globalState", JSON.stringify(state));
     },
   },
 });
