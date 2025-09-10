801fe5e24f0a641fe5ed7f9fdf55841465ed9b0c
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-09-10 08:00:04 +0000

    rrefactor: modify sql raw query to prisma

--- backend/src/controllers/user/infor.user.controllers.ts
+++ backend/src/controllers/user/infor.user.controllers.ts
@@ -1,31 +1,29 @@
 import express from "express";
-import { RequestCustom } from "src/types/index.types";
-import database from "src/database/index.database";
-import { Client } from "pg";
+
 import util from "src/utils/index.utils";
+import prisma from "src/models/prismaClient";
+import { RequestCustom } from "src/types/index.types";
 
 // #### CONTROLLER FUNCTIONS ####
 
 async function get(req: RequestCustom, res: express.Response) {
-    let db: Client | undefined = undefined;
-
     try {
-        const userId = req.user!.userId;
-        db = await database.getConnection();
-        const sql = `
-            SELECT username, email, address, phone_number
-            FROM users
-            WHERE userId = $1
-        `;
-        const result = await db.query(sql, [userId]);
-        const userInfo = result.rows[0];
+        const userInfo = await prisma.users.findUniqueOrThrow({
+            where: { userId: req.user?.userId },
+            select: {
+                username: true,
+                email: true,
+                address: true,
+                phoneNumber: true
+            }
+        });
+        if (!userInfo) {
+            return res.status(404).json(util.response.error('User not found'));
+        }
         res.status(200).json(util.response.success('Fetched user info successfully', { userInfor: userInfo }));
     } catch (error) {
         console.error("Error fetching user info:", error);
         res.status(500).json(util.response.internalServerError());
-    } finally {
-        if (db)
-            await database.releaseConnection(db);
     }
 }
 
