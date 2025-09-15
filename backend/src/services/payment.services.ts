84c58e33f8873fc350f9cd4aa8d3cf5a62d74a5b
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-09-15 09:59:49 +0000

    refactor: update MoMoMethod to accept amount as string and adjust amount calculation in create function

--- backend/src/services/payment.services.ts
+++ backend/src/services/payment.services.ts
@@ -2,13 +2,13 @@ import { PAYMENT_METHOD } from '@prisma/client';
 import axios from 'axios';
 import { CreatingOrderRequest } from 'src/types/order.types';
 
-async function MoMoMethod(order_code: string, amount: number, ) {
+async function MoMoMethod(order_code: string, amount: string) {
     var partnerCode = process.env.MOMO_PARTNER_CODE as string;
     var accessKey = process.env.MOMO_ACCESS_KEY as string;
     var secretKey = process.env.MOMO_SECRET_KEY as string;
     var orderInfo = 'pay with MoMo user hqh';
     var redirectUrl = '';
-    var ipnUrl = `${process.env.HOST_SERVER}${process.env.API_PREFIX}/user/payment/announce`;
+    var ipnUrl = `${process.env.BACK_END_URL}${process.env.API_PREFIX}/user/payment/announce`;
     var requestType = "payWithMethod";
     var orderId = partnerCode + '-' + order_code;
     var requestId = orderId;
@@ -18,6 +18,7 @@ async function MoMoMethod(order_code: string, amount: number, ) {
     var lang = 'vi';
     
     var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType;
+    console.log(rawSignature);
     const crypto = require('crypto');
     var signature = crypto.createHmac('sha256', secretKey)
         .update(rawSignature)
@@ -62,7 +63,7 @@ async function MoMoMethod(order_code: string, amount: number, ) {
 async function create(order_code: string, amount: number, paymentMethodCode: PAYMENT_METHOD) {
     switch (paymentMethodCode) {
         case PAYMENT_METHOD.MOMO:
-            return await MoMoMethod(order_code, amount * 1000);
+            return await MoMoMethod(order_code, String(Math.floor(amount*1000)));
         default:
             return {
                 paymentCode: order_code,
