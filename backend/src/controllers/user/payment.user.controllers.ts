d327323842a45c7af8d0efc393e14caeef8f7d0c
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-09-17 15:19:18 +0000

    refactor: migrate payment announcement to Prisma, removing PostgreSQL dependencies and streamlining database updates

--- backend/src/controllers/user/payment.user.controllers.ts
+++ backend/src/controllers/user/payment.user.controllers.ts
@@ -1,26 +1,17 @@
 import express from 'express';
-import database from 'src/database/index.database';
-import { Client } from 'pg';
 import { PAYMENT_STATUS } from '@prisma/client';
 import { RequestCustom } from 'src/types/common.types';
+import prisma from 'src/models/prismaClient';
 
 async function announce(req: RequestCustom, res: express.Response) {
-    // console.log('Payment announce:', req.body);
-    let db: Client|undefined = undefined;
     try {
-        db = await database.getConnection();
-        const sql = `
-            UPDATE payments
-            SET status = '${PAYMENT_STATUS.COMPLETED}'
-            WHERE payment_code = $1
-        `;
-        await db.query(sql, [req.body.requestId]);
+        await prisma.payments.update({
+            where: { paymentCode: req.body.paymentCode },
+            data: { status: PAYMENT_STATUS.COMPLETED }
+        });
+        console.log('Payment announced successfully');
     } catch (error) {
         console.log('Update database errors:', error);
-    } finally {
-        if (db) {
-            database.releaseConnection(db);
-        }
     }
     res.send();
 }
