6b66795b4e843c3c8d52dda8d6fb75f4803a8a96
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-09-17 10:45:06 +0000

    refactor: remove commented-out button code from AuthElements component

--- frontend/src/Components/Header/AuthElements.tsx
+++ frontend/src/Components/Header/AuthElements.tsx
@@ -3,9 +3,6 @@ import s from './Buttons.module.scss';
 
 const AuthElements = () => {
   return (
-    // <button onClick={handleSwitchToUserMode} className={s.backButton}>
-    //   Login
-    // </button>
     <Link to="/login" className={s.backButton}>Login</Link>
   );
 };
