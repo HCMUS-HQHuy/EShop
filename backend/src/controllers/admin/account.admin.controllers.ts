b2891c1322e1eef6766dfd552812f634adb421f7
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-09-17 14:39:27 +0000

    refactor: remove database connection logic and schema files, migrate to Prisma for database operations

--- backend/src/controllers/admin/account.admin.controllers.ts
+++ backend/src/controllers/admin/account.admin.controllers.ts
@@ -3,64 +3,12 @@ import { Client } from "pg";
 
 import util from "src/utils/index.utils";
 import schemas from "src/schemas/index.schema";
-import database from "src/database/index.database";
 
 import { SOCKET_EVENTS } from "src/constants/socketEvents";
 import { UpdateUserStatusRequest, UpdateSellerStatusRequest } from "src/types/index.types";
 import { RequestCustom } from "src/types/index.types";
 import {SHOP_STATUS, USER_STATUS} from "@prisma/client";
 import prisma from "src/models/prismaClient";
-
-// #### DATABASE FUNCTIONS ####
-
-async function checkUserCondition(data: UpdateUserStatusRequest) {
-    const errors: Partial<Record<keyof UpdateUserStatusRequest, string>> = {};
-    let db: Client | undefined = undefined;
-    try {
-        db = await database.getConnection();
-        const sql = `
-            SELECT COUNT(*) FROM users 
-            WHERE userId = $1 AND status = $2
-        `;
-        const status = data.status === USER_STATUS.BANNED ? USER_STATUS.ACTIVE : USER_STATUS.BANNED;
-        const result = await db.query(sql, [data.userId, status]);
-        if (parseInt(result.rows[0].count, 10) === 0) {
-            errors.userId = "User account not found or not banned";
-        }
-    } catch (error) {
-        console.error("Database error:", error);
-        throw new Error("Database validation failed");
-    } finally {
-        if (db) {
-            await database.releaseConnection(db);
-        }
-    }
-
-    return {
-        valid: Object.keys(errors).length === 0,
-        errors
-    };
-}
-
-async function updateUserStatus(userId: number, status: USER_STATUS) {
-    let db: Client | undefined = undefined;
-    try {
-        db = await database.getConnection();
-        const sql = `
-            UPDATE users SET status = $1
-            WHERE userId = $2
-        `;
-        const values = [status, userId];
-        await db.query(sql, values);
-    } catch (error) {
-        console.error("Error updating user status:", error);
-        throw new Error("Database error");
-    } finally {
-        if (db) {
-            await database.releaseConnection(db);
-        }
-    }
-}
 
 // #### CONTROLLER FUNCTIONS ####
 
@@ -70,83 +18,40 @@ async function list(req: RequestCustom, res: express.Response) {
     }
     let db: Client | undefined = undefined;
     try {
-        db = await database.getConnection();
-        const sql = `
-            SELECT u.userId, u.username, s.shopId, s.shop_name, s.status
-            FROM users AS u
-            LEFT JOIN shops AS s ON u.userId = s.userId
-            WHERE s.status = '${SHOP_STATUS.PENDING_VERIFICATION}'
-        `;
-        const result = await db.query(sql);
-        return res.status(200).json(result.rows);
+        const result = await prisma.users.findMany({
+            where: {
+                shop: {status: SHOP_STATUS.PENDING_VERIFICATION}
+            },
+            select: {
+                userId: true,
+                username: true,
+                shop: { select: { shopId: true, shopName: true, status: true } }
+            }
+        })
+        return res.status(200).json(util.response.success("Seller accounts fetched", {result}));
     } catch (error) {
         console.error("Error fetching seller accounts:", error);
         return res.status(500).json({ message: "Internal server error" });
-    } finally {
-        if (db) {
-            await database.releaseConnection(db);
-        }
     }
 }
 
-// This function handles the review of seller accounts by the admin
-// It validates the request data and updates the shop account status in the database
-// It updates the shop account status and optionally the rejection reason
 async function reviewShop(req: RequestCustom, res: express.Response) {
     if (util.role.isAdmin(req.user) === false) {
         return res.status(403).json(util.response.authorError('Admin'));
     }
-
-    // const getInfoShop = {}
-
-    // foreign key -> user -> userId
-    // check userId -> shop -> ok?
-
-    //--- 
-
-    // ok -> 
-
-    // const dataConfig = {
-    //     userId: getInfoShop.userId,
-    //     shopId: getInfoShop.shopId,
-    //     status: getInfoShop.status,
-    //     admin_note: getInfoShop.admin_note
-    // }
-
-    // io.emit(to [room, userId ] -> emmit  eventSOket (access shop / denied shop) -> dataConfig)
-
-    // --- 
-// not ok ->
-
-
-// ======
-
-// client
-
-// socket -> io.on(accessShop)
-
-// function handleAccessshop(data -> dataConfig của event) { 
-// check condition -> userId && shopId
-
-// true -> login / reload data -> show access/denied
-
-
-// }
-    // Validate the request body
     const parsedBody = schemas.shop.updateStatus.safeParse(req.body);
     if (!parsedBody.success) {
-        return res.status(400).send(util.response.error('Invalid request data', [util.formatError(parsedBody.error)]));
+        return res.status(400).send(util.response.zodValidationError(parsedBody.error));
     }
     const data: UpdateSellerStatusRequest = parsedBody.data;
 
-    // update the shop account status in the database
     try {
         const isShopExist = await prisma.shops.findUnique({
             where: { shopId: data.shopId },
             select: { shopId: true }
         });
         if (!isShopExist) {
-            return res.status(400).json(util.response.error('Invalid request', ['Shop does not exist']));
+            return res.status(400).json(util.response.error('Invalid request'));
         }
 
         const responseData = await prisma.shops.update({
@@ -160,16 +65,13 @@ async function reviewShop(req: RequestCustom, res: express.Response) {
         if (io === undefined) 
             throw Error("Socket IO instance is not available");
         io.to(`user_room_${data.userId}`).emit(SOCKET_EVENTS.SET_SHOP_STATUS, responseData);
-        return res.status(200).json(util.response.success("Shop account updated", [`shop :${data.shopId}`]));
+        return res.status(200).json(util.response.success("Shop account updated"));
     } catch (error) {
         console.error("Error updating shop account:", error);
         return res.status(500).json(util.response.error('Internal server error', ['Error updating shop account']));
     }
 }
 
-// This function handles the review of user accounts by the admin
-// It validates the request data and updates the user status in the database
-// It updates the user status to either 'Active' or 'Banned'
 async function reviewUser(req: RequestCustom, res: express.Response) {
     if (util.role.isAdmin(req.user) === false) {
         return res.status(403).json(util.response.authorError('Admin'));
@@ -177,27 +79,33 @@ async function reviewUser(req: RequestCustom, res: express.Response) {
     // Validate the request body
     const parsedBody = schemas.user.updateStatus.safeParse(req.body);
     if (!parsedBody.success) {
-        return res.status(400).send(util.response.error('Invalid request data', [util.formatError(parsedBody.error)]));
+        return res.status(400).send(util.response.zodValidationError(parsedBody.error));
     }
     const data: UpdateUserStatusRequest = parsedBody.data;
 
     // Check the user condition in the database
     try {
-        const conditionCheck = await checkUserCondition(data);
-        if (!conditionCheck.valid) {
-            return res.status(400).json(util.response.error('Validation error', [conditionCheck.errors]));
+        const user = await prisma.users.findUnique({
+            where: { userId: data.userId },
+            select: { status: true }
+        });
+        if (user === null) {
+            return res.status(400).json(util.response.error('Validation error'));
         }
     } catch (error) {
         console.error("Error checking user condition:", error);
-        return res.status(500).json(util.response.error('Internal server error during condition check', ['Error checking user condition']));
+        return res.status(500).json(util.response.internalServerError());
     }
 
     try {
-        await updateUserStatus(data.userId, data.status);
-        return res.status(200).json(util.response.success("User account updated", [data.userId]));
+        await prisma.users.update({
+            where: { userId: data.userId },
+            data: { status: data.status }
+        })
+        return res.status(200).json(util.response.success("User account updated"));
     } catch (error) {
         console.error("Error updating user account:", error);
-        return res.status(500).json(util.response.error('Internal server error', ['Error updating user account']));
+        return res.status(500).json(util.response.internalServerError());
     }
 }
 
